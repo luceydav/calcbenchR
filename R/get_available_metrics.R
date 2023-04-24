83d44851f75460942289b31747c0e610cf60aa64
David Lucey <dnl2001@stern.nyu.edu>
2023-04-24 08:46:46 -0400

    add get_available_metrics

---
+++ R/get_available_metrics.R
@@ -0,0 +1,23 @@
+
+#' @title Get Available Metrics
+#'
+#' @description Function to get Calcbench available metrics
+#'
+#' @return data.table
+#' 
+#' @export
+get_available_metrics <- function() {
+  
+  api_client <-
+    ApiClient$new(
+      email = Sys.getenv("CALCBENCH_USERNAME"),
+      strng = Sys.getenv("CALCBENCH_PASSWORD")
+    )
+  
+  # Get metrics
+  metrics_api <- AvailableMetricsApi$new(api_client = api_client)
+  metrics <- metrics_api$AvailableMetricsGetValues()
+  
+  return(metrics)
+  
+}
